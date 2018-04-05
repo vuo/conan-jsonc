d737cb8fbbfaaa85c83fad32334718101b4aec22
Steve Mokris <smokris@softpixel.com>
2018-04-05 19:42:12 -0400

    Ensure we don't link to libstdc++.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -19,8 +19,10 @@ class JsoncTestConan(ConanFile):
         # Ensure we only link to system libraries and our own libraries.
         if platform.system() == 'Darwin':
             self.run('! (otool -L lib/*.dylib | grep -v "^lib/" | egrep -v "^\s*(/usr/lib/|/System/|@rpath/)")')
+            self.run('! (otool -L lib/*.dylib | fgrep "libstdc++")')
             self.run('! (otool -l lib/*.dylib | grep -A2 LC_RPATH | cut -d"(" -f1 | grep "\s*path" | egrep -v "^\s*path @(executable|loader)_path")')
         elif platform.system() == 'Linux':
             self.run('! (ldd lib/libjson-c.so | grep "/" | egrep -v "(\s(/lib64/|(/usr)?/lib/x86_64-linux-gnu/)|test_package/build)")')
+            self.run('! (ldd lib/libjson-c.so | fgrep "libstdc++")')
         else:
             raise Exception('Unknown platform "%s"' % platform.system())
