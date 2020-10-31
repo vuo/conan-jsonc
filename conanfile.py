9ca9109f2c5240016088331fe83bb29b831d4122
Steve Mokris <steve@kosada.com>
2020-10-31 14:06:52 -0400

    Update to json-c 0.15 (latest).  Build for both x86_64 + arm64.  [kosada/vuo/vuo#17836]

--- conanfile.py
+++ conanfile.py
@@ -1,22 +1,32 @@
-from conans import ConanFile, tools, AutoToolsBuildEnvironment
+from conans import ConanFile, CMake, tools
 import os
 import platform
 
 class JsoncConan(ConanFile):
     name = 'jsonc'
 
-    source_version = '0.12'
-    source_fullversion = '0.12-20140410'
-    package_version = '3'
+    source_version = '0.15'
+    source_fullversion = '0.15-20200726'
+    package_version = '0'
     version = '%s-%s' % (source_version, package_version)
 
-    build_requires = 'llvm/3.3-5@vuo/stable'
+    build_requires = (
+        'llvm/5.0.2-1@vuo/stable',
+        'macos-sdk/11.0-0@vuo/stable',
+        'vuoutils/1.2@vuo/stable',
+    )
     settings = 'os', 'compiler', 'build_type', 'arch'
     url = 'https://github.com/vuo/conan-jsonc'
     license = 'https://github.com/json-c/json-c/blob/master/COPYING'
     description = 'Construct JSON objects in C, output them as JSON formatted strings and parse JSON formatted strings back into the C representation of JSON objects'
     source_dir = 'json-c-json-c-%s' % source_fullversion
+
     build_dir = '_build'
+    install_dir = '_install'
+
+    libs = {
+        'json-c': 5,
+    }
 
     def requirements(self):
         if platform.system() == 'Linux':
@@ -26,43 +36,34 @@ class JsoncConan(ConanFile):
 
     def source(self):
         tools.get('https://github.com/json-c/json-c/archive/json-c-%s.tar.gz' % self.source_fullversion,
-                  sha256='99304a4a633f1ee281d6a521155a182824dd995139d5ed6ee5c93093c281092b')
+                  sha256='4ba9a090a42cf1e12b84c64e4464bb6fb893666841d5843cc5bef90774028882')
 
         self.run('mv %s/COPYING %s/%s.txt' % (self.source_dir, self.source_dir, self.name))
 
     def build(self):
+        import VuoUtils
+
+        cmake = CMake(self)
+
+        cmake.definitions['CMAKE_BUILD_TYPE'] = 'Release'
+        cmake.definitions['CMAKE_C_COMPILER']   = '%s/bin/clang'   % self.deps_cpp_info['llvm'].rootpath
+        cmake.definitions['CMAKE_C_FLAGS'] = '-Oz'
+        cmake.definitions['CMAKE_INSTALL_NAME_DIR'] = '@rpath'
+        cmake.definitions['CMAKE_INSTALL_PREFIX'] = '%s/%s' % (os.getcwd(), self.install_dir)
+        cmake.definitions['CMAKE_OSX_ARCHITECTURES'] = 'x86_64;arm64'
+        cmake.definitions['CMAKE_OSX_DEPLOYMENT_TARGET'] = '10.11'
+        cmake.definitions['CMAKE_OSX_SYSROOT'] = self.deps_cpp_info['macos-sdk'].rootpath
+        cmake.definitions['BUILD_SHARED_LIBS'] = 'ON'
+        cmake.definitions['BUILD_STATIC_LIBS'] = 'OFF'
         tools.mkdir(self.build_dir)
         with tools.chdir(self.build_dir):
-            autotools = AutoToolsBuildEnvironment(self)
+            cmake.configure(source_dir='../%s' % self.source_dir,
+                            build_dir='.')
+            cmake.build()
+            cmake.install()
 
-            # The LLVM/Clang libs get automatically added by the `requires` line,
-            # but this package doesn't need to link with them.
-            autotools.libs = ['c++abi']
-
-            autotools.flags.append('-Oz')
-            autotools.flags.append('-Wno-error')
-
-            if platform.system() == 'Darwin':
-                autotools.flags.append('-mmacosx-version-min=10.10')
-                autotools.link_flags.append('-Wl,-headerpad_max_install_names')
-                autotools.link_flags.append('-Wl,-install_name,@rpath/libjson-c.dylib')
-
-            env_vars = {
-                'CC' : self.deps_cpp_info['llvm'].rootpath + '/bin/clang',
-                'CXX': self.deps_cpp_info['llvm'].rootpath + '/bin/clang++',
-            }
-            with tools.environment_append(env_vars):
-                autotools.configure(configure_dir='../%s' % self.source_dir,
-                                    build=False,
-                                    host=False,
-                                    args=['--quiet',
-                                          '--prefix=%s' % os.getcwd()])
-                autotools.make(args=['--quiet'])
-                autotools.make(target='install', args=['--quiet'])
-
-            if platform.system() == 'Linux':
-                patchelf = self.deps_cpp_info['patchelf'].rootpath + '/bin/patchelf'
-                self.run('%s --set-soname libjson-c.so lib/libjson-c.so' % patchelf)
+        with tools.chdir('%s/lib' % self.install_dir):
+            VuoUtils.fixLibs(self.libs, self.deps_cpp_info)
 
     def package(self):
         if platform.system() == 'Darwin':
@@ -72,8 +73,8 @@ class JsoncConan(ConanFile):
         else:
             raise Exception('Unknown platform "%s"' % platform.system())
 
-        self.copy('*.h', src='%s/include' % self.build_dir, dst='include')
-        self.copy('libjson-c.%s' % libext, src='%s/lib' % self.build_dir, dst='lib')
+        self.copy('*.h', src='%s/include' % self.install_dir, dst='include')
+        self.copy('libjson-c.%s' % libext, src='%s/lib' % self.install_dir, dst='lib')
 
         self.copy('%s.txt' % self.name, src=self.source_dir, dst='license')
 
